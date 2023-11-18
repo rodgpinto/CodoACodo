8b7a57fff80800a05ee8b2006640f47397e19fc7
Rodrigo Pinto <114892008+rodgpinto@users.noreply.github.com>
2023-11-18 12:25:47 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,17 +1,12 @@
 function calcularPrecio() {
-    // Obtener el valor de la cantidad desde el input
     var cantidad = document.getElementById('cantidad').value;
 
-    // Valor constante por unidad del producto
     var valorTicket = 200;
 
-    // Calcular el precio total sin descuento
     var precioTotal = cantidad * valorTicket;
 
-    // Obtener el valor del descuento seleccionado
     var descuentoSeleccionado = document.getElementById('descuento').value;
 
-    // Aplicar el descuento correspondiente
     var precioConDescuento;
     if (descuentoSeleccionado === 'estudiante') {
         precioConDescuento = precioTotal * 0.2; // 80% de descuento
@@ -22,10 +17,8 @@ function calcularPrecio() {
         precioConDescuento = precioTotal * 0.85; // 15% de descuento
     }
 
-    // Mostrar el resultado en la etiqueta con id "resultado"
     document.getElementById('resultado').innerHTML = 'Total a Pagar:$' + precioConDescuento.toFixed(2);
 }
-// Limpiar el contenido del elemento con id "resultado"
 function limpiarResultado() {
 
     document.getElementById('resultado').innerHTML = 'Total a Pagar:$';
